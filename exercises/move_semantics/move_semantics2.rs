2d5e4213015c4152850879f57d5aefd15bd7f452
murku <downquark@gmail.com>
2023-01-29 16:17:06 +0000

    move_semantics2 sol3

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -5,9 +5,9 @@
 // I AM NOT DONE
 
 fn main() {
-    let mut vec0 = Vec::new();
+    let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(&mut vec0);
+    let mut vec1 = fill_vec(&vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -17,7 +17,7 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: &mut Vec<i32>) -> Vec<i32> {
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
     let mut vec = vec.to_vec();
 
     vec.push(22);
